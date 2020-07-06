6b2478a76d3b68ea2a8beacfa5e90a578b48829e
McCuffin <gamingpurpose791@gmail.com>
2020-07-06 08:03:47 +0530

    Minor Updates

--- app/src/main/java/com/example/basic_mapbox/MainActivity.java
+++ app/src/main/java/com/example/basic_mapbox/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.basic_mapbox;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.BitmapFactory;
-import android.os.Build;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
 import com.mapbox.geojson.Feature;
-import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
@@ -21,59 +26,35 @@ import com.mapbox.mapboxsdk.maps.Style;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, PermissionsListener {
 
     private MapView mapView;
+    private MapboxMap mapboxMap;
+    private Location yourLocation;
+    private PermissionsManager permissionsManager;
     private static final int LOCATION_PERMISSION = 99;
     private static final String SOURCE_ID = "SOURCE_ID";
     private static final String ICON_ID = "ICON_ID";
     private static final String LAYER_ID = "LAYER_ID";
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public void locationPermission()
-    {
-        if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_DENIED)
-        {
-            //TODO:Have to show the reason for taking any permission.
-
-            /* AlertDialog.Builder reasonOfPerms = new AlertDialog.Builder(this);
-            reasonOfPerms.setTitle(getString(R.string.user_location_permission_title));
-            reasonOfPerms.setMessage(getString(R.string.user_location_permission_explanation));
-            reasonOfPerms.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    //Needed to be left empty since nothing is to be done.
-                }
-            });
-            AlertDialog permsDialog = reasonOfPerms.create();
-            permsDialog.show();
-            */
-
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION);
-
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, getString(R.string.access_token));
         setContentView(R.layout.activity_main);
-        locationPermission();
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
     }
 
     @Override
+    @SuppressWarnings( {"MissingPermission"})
     public void onStart() {
         super.onStart();
         mapView.onStart();
@@ -116,8 +97,95 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onMapReady(@NonNull MapboxMap mapboxMap) {
-        List<Feature> symbolLayerIconFeatureList = new ArrayList<>();
+    public void onMapReady(@NonNull final MapboxMap mapboxMap) {
+        MainActivity.this.mapboxMap = mapboxMap;
+
+        mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/mapbox/streets-v11"),
+                new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+                        enableLocationComponent(style);
+                    }
+                });
+    }
+
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+// Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+// Get an instance of the component
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+
+// Activate with options
+            locationComponent.activateLocationComponent(
+                    LocationComponentActivationOptions.builder(this, loadedMapStyle).build());
+
+// Enable to make component visible
+            locationComponent.setLocationComponentEnabled(true);
+//Getting current location of user
+            yourLocation = locationComponent.getLastKnownLocation();
+            Feature currLocation = Feature.fromGeometry(Point.fromLngLat(yourLocation.getLongitude(),yourLocation.getLatitude()));
+            loadedMapStyle.addImage(ICON_ID, BitmapFactory.decodeResource(MainActivity.this.getResources(), R.drawable.mapbox_marker_icon_default));
+            loadedMapStyle.addSource(new GeoJsonSource(SOURCE_ID, currLocation));
+            loadedMapStyle.addLayer(new SymbolLayer(LAYER_ID, SOURCE_ID)
+            .withProperties(
+                    iconImage(ICON_ID),
+                    iconAllowOverlap(true),
+                    iconIgnorePlacement(true)
+            )
+            );
+            // Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+
+// Set the component's render mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        AlertDialog.Builder reasonOfPerms = new AlertDialog.Builder(this);
+        reasonOfPerms.setTitle(getString(R.string.user_location_permission_title));
+        reasonOfPerms.setMessage(getString(R.string.user_location_permission_explanation));
+        reasonOfPerms.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //Needed to be left empty since nothing is to be done.
+            }
+        });
+        AlertDialog permsDialog = reasonOfPerms.create();
+        permsDialog.show();
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(style);
+                }
+            });
+        }
+        else {
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+}
+/*
+
+ todo : Earlier mapReady which showed pins
+ List<Feature> symbolLayerIconFeatureList = new ArrayList<>();
         symbolLayerIconFeatureList.add(Feature.fromGeometry(
                 Point.fromLngLat(-57.225365, -33.213144)));
         symbolLayerIconFeatureList.add(Feature.fromGeometry(
@@ -125,10 +193,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         symbolLayerIconFeatureList.add(Feature.fromGeometry(
                 Point.fromLngLat(-56.990533, -30.583266)));
 
-        mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/mapbox/streets-v10")
-        //TODO: Check and change mapbox styles as per day and night
-        //TODO: Get user location values in order to show its current pin on map
-        //TODO: Search bar working as per location being entered
+        mapboxMap.setStyle(new Style.Builder().fromUri("mapbox://styles/mapbox/cjerxnqt3cgvp2rmyuxbeqme7")
+
 // Add the SymbolLayer icon image to the map style
                 .withImage(ICON_ID, BitmapFactory.decodeResource(MainActivity.this.getResources(), R.drawable.mapbox_marker_icon_default))
 
@@ -153,5 +219,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 // Map is set up and the style has loaded. Now you can add additional data or make other map adjustments.
             }
         });
-    }
-}
+
+todo :
+*/
